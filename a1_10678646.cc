911b8cda6042f626d5c4addd706c95260d5143ae
Akekua <42778442+Akekua@users.noreply.github.com>
2018-08-29 19:44:30 +0100

    Update a1_10678646.cc
    assignment complete

--- a1_10678646.cc
+++ a1_10678646.cc
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num, r;
+  bool isPrime = true;
+
+  cout << "Enter a positive integer: ";
+  cin >> num;
+
+  for(r = 2; r <= num / 2; ++r)
+  {
+      if(num % r == 0)
+      {
+          isPrime = false;
+          break;
+      }
+
+
+  }
+  if (isPrime)
+      cout << "The number "<< num << " is a prime number";
+  else
+      cout << "The number " << num << " is not a prime number";
+
+  return 0;
+}
